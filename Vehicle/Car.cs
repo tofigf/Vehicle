2cd81ed633c03118d4f672dc64a35ce573580669
tofigf <tofigshf@code.edu.az>
2018-03-09 23:14:35 +0400

    vehicle

---
+++ Vehicle/Car.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vehicle
+{
+    class Car
+    {
+        public string Marka { get; set; }
+        public double Capacity { get; set; }
+        public double UseLitr { get; set; }
+        private double Reallitr { get; set; }
+        private double Global { get; set; }
+      
+        public Car (string Marka, double Capacity, double UseLitr, double Reallitr, double Global)
+        {
+            this.Marka = Marka;
+            this.Capacity = Capacity;
+            this.UseLitr = UseLitr;
+            this.Reallitr = Reallitr;
+            this.Global = Global;
+
+        }
+        double calcyol = 0;
+        public void Drive (double yol)
+        {
+            double btnyol = this.UseLitr * this.Reallitr;
+            if(yol<=btnyol)
+            {
+                double gedebilmesa = btnyol - yol;
+                double qalanbenzin = (btnyol - yol) / this.UseLitr;
+                this.calcyol += yol;
+                this.Global += yol;
+                this.Reallitr = qalanbenzin;
+                Console.WriteLine("You can go this distance with the fuel in the tank." );
+                Console.WriteLine("gasoline remain : "+ qalanbenzin +" litr");
+                Console.WriteLine("Distance you can go " + gedebilmesa + " km" );
+
+            }
+            else
+            {
+                Console.WriteLine("You can not go this distance with fuel in your own." );
+                Console.WriteLine("Maxsimum distance you can go " + btnyol + " km");
+            }
+        }
+
+        public void Tank(double fill)
+        {
+            double ferq = this.Capacity - this.Reallitr;
+            if(fill <ferq)
+            {
+                double plusgas = this.Reallitr + fill;
+                this.Reallitr = plusgas;
+                Console.WriteLine("Uploaded Capacity: " + plusgas + " litr" +  " Capacity of car:"+ this.Capacity+"litr");
+            }
+            else
+            {
+                Console.WriteLine("This is bigger than the capacity of tank car.");
+                Console.WriteLine("The current level "+ this.Reallitr + "litr");
+                Console.WriteLine("At the moment can be entered "+ ferq +"litr");
+            }
+        } public void Calcyol()
+        {
+            Console.WriteLine("all driving distance" + this.calcyol+" km");
+        }
+        public void Delete()
+        {
+            this.calcyol = 0;
+            Console.WriteLine("Trip reseted:" + this.calcyol + "km");
+        }
+        public void Calcodometr()
+        {
+            Console.WriteLine("At the moment:"+ this.Global +" km");
+        }
+
+
+    }
+}
---
+++ Vehicle/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vehicle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car cr = new Car("BMW", 60, 10, 20, 7777);
+          
+            string select;
+            do
+            {
+
+                Console.WriteLine("1. Drive");
+                Console.WriteLine("2.Filling the tank");
+                Console.WriteLine("3. All yours  local distance");
+                Console.WriteLine("4. Global odometr");
+                Console.WriteLine("5. Reset yours local distance");
+                Console.WriteLine("0. Exit");
+                select = Console.ReadLine();
+                switch (Convert.ToInt32(select))
+                {
+
+                    case 1:
+                        Console.WriteLine("\n");
+                        Console.WriteLine("Please write your going distance:");
+                    
+                        string mesafe = Console.ReadLine();
+                        double Mesafe = 0;
+                        while (!double.TryParse(mesafe, out Mesafe))
+                        {
+                            Console.WriteLine("\n");
+                            Console.WriteLine("you cannot write such");
+                            mesafe = Console.ReadLine();
+                        }
+                        Console.WriteLine("\n");
+                        cr.Drive(Mesafe);
+                        Console.WriteLine("\n");
+                        break;
+                    case 2:
+                        Console.WriteLine("\n");
+                        Console.WriteLine("how many litr do you want add?:");
+                        string miqdar = Console.ReadLine();
+                        double Miqdar = 0;
+                        while (!double.TryParse(miqdar, out Miqdar))
+                        {
+                            Console.WriteLine("\n");
+                            Console.WriteLine("you cannot write such");
+                            miqdar = Console.ReadLine();
+                        }
+                        Console.WriteLine("\n");
+                        cr.Tank(Miqdar);
+                        Console.WriteLine("\n");
+                        break;
+                    case 3:
+                        Console.WriteLine("\n");
+                        cr.Calcyol();
+                        Console.WriteLine("\n");
+                        break;
+                   
+                    case 4:
+                        Console.WriteLine("\n");
+                        cr.Calcodometr();
+                        Console.WriteLine("\n");
+                        break;
+                    case 5:
+                        Console.WriteLine("\n");
+                        cr.Delete();
+                        Console.WriteLine("\n");
+                        break;
+                    case 6:
+                        Console.WriteLine("\n");
+                      
+                        Console.WriteLine("\n");
+                        break;
+                    case 7:
+                        Console.WriteLine("\n");
+
+                        Console.WriteLine("\n");
+                        break;
+                    case 8:
+                        Console.WriteLine("\n");
+
+                        Console.WriteLine("\n");
+                        break;
+                    default:
+                        break;
+                }
+            } while (Convert.ToInt32(select) != 0);
+        }
+    }
+}
